962c96fd94558aceb7033909f238e7aa3d709025
Liam <liamwears@fastmail.com>
2023-09-19 11:36:32 +0200

    Initial commit

--- app.js
+++
@@ -1,8 +0,0 @@
-http = require('http');
-
-server = http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('PoC by GoDiego (<a href=https://hackerone.com/diegobernal>https://hackerone.com/diegobernal</a>)');
-    response.end();
-});
-server.listen(80);
